d341984dac141a958cbf8fe17a0721fbb17c16e5
Alexandr Krivosheya <a.krivosheya@g.nsu.ru>
2022-10-26 15:59:44 +0700

    Added ContentGenerator

---
+++ ContenderGenerator.cs
@@ -0,0 +1,49 @@
+using System.Text;
+
+namespace PrincessConsole
+{
+    class ContenderGenerator
+    {
+        private const int MaxNameLength = 10;
+        private const int MinNameLength = 1;
+
+        public void GenerateContenders(string[] queue, Dictionary<string, Aspirant> aspirants)
+        {
+            for(int i = 0; i < queue.Length; ++i)
+            {
+                string randomName = GetRandomName();
+                while(aspirants.ContainsKey(randomName)){
+                    randomName = GetRandomName();
+                }
+                aspirants.Add(randomName, new Aspirant { Name=randomName, Quality=(i + 1) });
+                queue[i] = randomName;
+            }
+            ShuffleArray(queue);
+        }
+
+        private string GetRandomName()
+        {
+            Random random = new Random();
+            int nameLength = random.Next(MinNameLength, MaxNameLength);
+            StringBuilder name = new StringBuilder(nameLength);
+            for(int i = 0; i < nameLength; ++i)
+            {
+                char nextSymb = (char)random.Next((int)'a', (int)'z');
+                name.Append(nextSymb);
+            }
+            return name.ToString();
+        }
+
+        private void ShuffleArray(string[] array)
+        {
+            Random random = new Random();
+            for(int i = 0; i < array.Length; ++i)
+            {
+                string tmp = array[i];
+                int r = random.Next(i, array.Length);
+                array[i] = array[r];
+                array[r] = tmp;
+            }
+        }
+    }
+}
