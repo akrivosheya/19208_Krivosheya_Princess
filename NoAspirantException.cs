0e341ebb44634b29cf9988b882c932e77dd7d805
Alexandr Krivosheya <a.krivosheya@g.nsu.ru>
2022-11-01 15:49:02 +0700

    Add exceptions for Friend and Hall

---
+++ NoAspirantException.cs
@@ -0,0 +1,10 @@
+namespace PrincessConsole
+{
+    public class NoAspirantException : Exception
+    {
+        public NoAspirantException(string message) : base(message)
+        {
+
+        }
+    }
+}
---
+++ StrangerAspirantException.cs
@@ -0,0 +1,10 @@
+namespace PrincessConsole
+{
+    public class StrangerAspirantException : Exception
+    {
+        public StrangerAspirantException(string message) : base(message)
+        {
+
+        }
+    }
+}
